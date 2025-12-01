284699f64a8caf705a0ca7c7aa15c171dc46aae8
Nazar <popadyc123nazar@gmail.com>
2025-12-01 16:40:27 +0000

    valid=>allow

--- src/validations/noteValidation.js
+++ src/validations/noteValidation.js
@@ -8,7 +8,7 @@ export const getAllNotesSchema = {
     [Segments.QUERY]: Joi.object({
         page: Joi.number().integer().min(1).default(1),
         perPage: Joi.number().integer().min(5).max(20).default(10),
-        tag: Joi.string().valid(...TAGS),
+        tag: Joi.string().allow(...TAGS),
         search: Joi.string().trim().allow('')
     })
 };
@@ -27,7 +27,7 @@ export const createNoteSchema = {
     [Segments.BODY]: Joi.object({
         title: Joi.string().min(1).required(),
         content: Joi.string().trim().allow(''),
-        tag: Joi.string().valid(TAGS),
+        tag: Joi.string().allow(TAGS),
 })
 };
 
@@ -38,6 +38,6 @@ export const updateNoteSchema = {
     [Segments.BODY]: Joi.object({
         title: Joi.string().min(1).required(),
         content: Joi.string().trim().allow(''),
-        tag: Joi.string().valid(...TAGS),
+        tag: Joi.string().allow(...TAGS),
 }).min(1),
 };
