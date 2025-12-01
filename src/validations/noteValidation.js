f87140f742c97d486c952a85eb2467b4567b115a
Nazar <popadyc123nazar@gmail.com>
2025-12-01 16:33:12 +0000

    fourth save

--- src/validations/noteValidation.js
+++ src/validations/noteValidation.js
@@ -8,7 +8,7 @@ export const getAllNotesSchema = {
     [Segments.QUERY]: Joi.object({
         page: Joi.number().integer().min(1).default(1),
         perPage: Joi.number().integer().min(5).max(20).default(10),
-        tag: Joi.string().valid(TAGS),
+        tag: Joi.string().valid(...TAGS),
         search: Joi.string().trim().allow('')
     })
 };
@@ -38,6 +38,6 @@ export const updateNoteSchema = {
     [Segments.BODY]: Joi.object({
         title: Joi.string().min(1).required(),
         content: Joi.string().trim().allow(''),
-        tag: Joi.string().value(TAGS),
+        tag: Joi.string().value(...TAGS),
 }).min(1),
 };
